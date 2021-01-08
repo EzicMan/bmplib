78266d4c3a65136f3764628f459f6519c7e9f13a
Александр <axew165@gmail.com>
2021-01-08 09:35:01 +0300

    SomeOrdinaryFixes
    Added some options

--- main.cpp
+++ main.cpp
@@ -8,11 +8,17 @@ using namespace std;
 
 int main()
 {
-	int x, y;
-	x = 1600;
-	y = 1600;
-	bitMapImage myImage(x,y);
 	srand(time(NULL));
+	int x, y;
+	x = 315;
+	y = 315;
+	/*Color colors[16];
+	for (int i = 0; i < 8; i++) {
+		colors[0 + i] = Color(i & 1 ? 128 : 0, i & 2 ? 128 : 0, i & 4 ? 128 : 0);
+		colors[8 + i] = Color(i & 1 ? 255 : 0, i & 2 ? 255 : 0, i & 4 ? 255 : 0);
+	}*/
+	bitMapImage<4294967295> myImage(x,y);
+	/*srand(time(NULL));
 	int t = 0;
 	for (int i = 0; i < 16; i++) {
 		for (int j = 0; j < 16; j++) {
@@ -22,6 +28,11 @@ int main()
 				}
 			}
 			t++;
+		}
+	}*/
+	for (int i = 0; i < y; i++) {
+		for (int j = 0; j < x; j++) {
+			myImage.setPixel(j, i, rand()%16);
 		}
 	}
 	myImage.saveToFile("test.bmp");
